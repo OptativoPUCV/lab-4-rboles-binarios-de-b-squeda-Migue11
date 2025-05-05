6817cabf5fc24be18a5af6317d237729c79ce833
Your Name <you@example.com>
2025-05-05 06:59:53 -0400

    new try

--- treemap.c
+++ treemap.c
@@ -98,15 +98,15 @@ void removeNode(TreeMap * tree, TreeNode* node){
 }
 
 void eraseTreeMap(TreeMap * tree, void* key){
-    if (tree == NULL || tree->root == NULL){
-        return;
-    }
+    // if (tree == NULL || tree->root == NULL){
+    //     return;
+    // }
 
-    if (searchTreeMap(tree, key) == NULL){
-        return;
-    }
-    TreeNode* node = tree->current;
-    removeNode(tree, node);
+    // if (searchTreeMap(tree, key) == NULL){
+    //     return;
+    // }
+    // TreeNode* node = tree->current;
+    // removeNode(tree, node);
 
 }
 
